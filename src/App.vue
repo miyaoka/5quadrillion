0e1a81d6854f5685e3badb4a08603804105d9c53
Masaya Kazama <miyan@t-p.jp>
2017-06-22 14:50:03 +0900

    Add vue-material

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,8 @@ export default {
 </script>
 
 <style>
+@import '../node_modules/vue-material/dist/vue-material.css';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
