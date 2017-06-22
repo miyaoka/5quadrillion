db089ff7ed59cd82722186a9fbcc45ff68792ca0
Masaya Kazama <miyan@t-p.jp>
2017-06-22 18:02:07 +0900

    Add price component

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,29 +1,35 @@
 <template>
   <div>
-    <money-number
-      :num="num"
-    ></money-number>
+    <price
+      :exponent="exponent"
+      :base="base"
+    ></price>
     <money-slider
-      v-model="num"
-      :max="5000000000000000"
+      v-model="exponent"
+      :max="expoMax"
       :isMobile="true"
     ></money-slider>
   </div>
 </template>
 
 <script>
-import MoneyNumber from '@/components/MoneyNumber'
+import Price from '@/components/Price'
 import MoneySlider from '@/components/MoneySlider'
+
+const base = 10000
+const max = 5000000000000000
 
 export default {
   name: 'main',
   components: {
-    MoneyNumber,
+    Price,
     MoneySlider
   },
   data () {
     return {
-      num: 10000
+      base: base,
+      exponent: 1,
+      expoMax: Math.log(max) / Math.log(base)
     }
   }
 }
@@ -31,21 +37,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
---
+++ src/components/Price.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div>
+      <img
+        v-for="num in nums"
+        :src="numImgPath(num)"
+        class="price"
+      >
+      <span class="unit">
+        <img
+          v-if="this.exponent >= 1"
+          :src="baseImgPath(exponent)"
+        >
+        <img src="../assets/img/currency/yen.png">
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    exponent: Number,
+    base: Number
+  },
+  methods: {
+    numImgPath (num) {
+      return require(`@/assets/img/number/${num}.png`)
+    },
+    baseImgPath (exponent) {
+      return require(`@/assets/img/unit/base10000/${Math.floor(exponent)}.png`)
+    },
+    currencyImgPath (exponent) {
+      return require(`@/assets/img/unit/base10000/${Math.floor(exponent)}.png`)
+    }
+  },
+  computed: {
+    nums () {
+      return String(Math.ceil(Math.pow(this.base, this.exponent % 1))).split('')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.price {
+  margin: 0 -10px;
+}
+.unit img{
+  margin: 0 -20px;
+}
+</style>
