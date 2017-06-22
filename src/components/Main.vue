873e89228a36173fecb571a26ab27fd0674ee9c8
Masaya Kazama <miyan@t-p.jp>
2017-06-22 16:06:43 +0900

    Add slider component

---
+++ src/components/Main.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <money-number
+      :num="num"
+    ></money-number>
+    <money-slider
+      v-model="num"
+      :max="5000000000000000"
+      :isMobile="true"
+    ></money-slider>
+  </div>
+</template>
+
+<script>
+import MoneyNumber from '@/components/MoneyNumber'
+import MoneySlider from '@/components/MoneySlider'
+
+export default {
+  name: 'main',
+  components: {
+    MoneyNumber,
+    MoneySlider
+  },
+  data () {
+    return {
+      num: 10000
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/MoneySlider.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <vue-slider
+      ref="slider"
+      :value="value"
+      @input="$emit('input', $event)"
+      :max="max"
+      :isMobile="isMobile"
+      :reverse="true"
+      :height="24"
+      :dotSize="50"
+      :speed=".5"
+      :processStyle = "{
+        backgroundImage: '-webkit-linear-gradient(left, #ff0000, #3498db)'
+      }"
+    ></vue-slider>
+    {{isMobile}}
+  </div>
+</template>
+
+<script>
+import vueSlider from 'vue-slider-component'
+
+export default {
+  components: {
+    vueSlider
+  },
+  props: {
+    value: Number,
+    max: Number
+  },
+  data () {
+    return {
+      val: this.value,
+      isMobile: window.ontouchstart
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
