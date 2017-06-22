8bcf8b4cc8991fe4753f2ff2a394296902996a20
Masaya Kazama <miyan@t-p.jp>
2017-06-22 23:20:45 +0900

    Remove tooltip on slider

--- src/components/MoneySlider.vue
+++ src/components/MoneySlider.vue
@@ -11,6 +11,7 @@
       :dotSize="50"
       :speed=".5"
       :interval=".0001"
+      :tooltip="false"
       :processStyle = "{
         backgroundImage: '-webkit-linear-gradient(left, #ff0000, #3498db)'
       }"
